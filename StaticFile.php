54ee2077624dcce2850861a76b6e40313e643f06
Ethan <ethan.mail0417@gmail.com>
2014-03-26 11:18:27 +0800

    add static file and page.

---
+++ StaticFile.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+  </head>
+  <body>
+    <div>
+      <img src="images/taiwan.png" width='400px'>
+    </div>
+    <div>
+      <p>
+        這是測試文字
+      </p>
+    </div>
+  </body>
+</html>
